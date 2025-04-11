fb99ef506b89dd29d993ca4189aff40e8c23f209
Morten la Cour <lacour@gmail.com>
2025-04-11 11:19:56 +0200

    servicebsu

---
+++ Courses/20250409/servicebus/falck.servicebusreceiver/Program.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+
+string sbNamespace = "falckmlc";
+string topicName = "mytopic";
+string subscriptionName = "getall";
+
+int maxMessages = 3;
+var maxWaitTime = new TimeSpan(0,0,20); //Special!!
+
+string connectionString = "Endpoint=sb://falckml......";
+//Service Bus Namespace / Settings / Shared Access Sig / Click on Root.... Copy the connectionString
+
+//var credential = new AzureCliCredential();
+
+// var client = new ServiceBusClient(
+//         fullyQualifiedNamespace: $"{sbNamespace}.servicebus.windows.net",
+//         credential: credential);
+
+var client = new ServiceBusClient(connectionString);
+
+
+var options = new ServiceBusReceiverOptions
+{
+    ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete
+};
+
+var receiver = client.CreateReceiver(
+        topicName: topicName,
+        subscriptionName: subscriptionName,
+        options: options);
+
+var messages = await receiver
+            .ReceiveMessagesAsync(
+                maxMessages: maxMessages,
+                maxWaitTime: maxWaitTime);
+
+foreach(var message in messages)
+{
+    System.Console.WriteLine(message.Body.ToString());
+}
---
+++ Courses/20250409/servicebus/falck.servicebussubmitter/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//Client ???
+
+using Azure.Identity;
+using Azure.Messaging.ServiceBus;
+
+string sbNamespace = "falckmlc";
+string topicName = "mytopic";
+string country = args[0];
+
+var credential = new AzureCliCredential();
+
+var client = new ServiceBusClient(
+        fullyQualifiedNamespace: $"{sbNamespace}.servicebus.windows.net",
+        credential: credential);
+
+
+var sender = client.CreateSender(topicName);
+
+var message = new ServiceBusMessage("Hello from the app");
+message.ApplicationProperties.Add("country",country);
+
+await sender.SendMessageAsync(message);
+System.Console.WriteLine("Message sent...");
+
